0c64151d038ab3ebc6eec052cdee62465263ab34
Gabriel Azarya <gabrielkrishna5@gmail.com>
2024-04-24 01:09:42 +0700

    Update server.py

--- No1/server.py
+++ No1/server.py
@@ -1,11 +1,26 @@
 import socket
 import ssl
+import os
 
 HOST = 'localhost'
 PORT = 12345
 
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile='certs/server.crt', keyfile='certs/server.key')
+
+certfile_path_1 = 'No1/certs/server.crt'
+keyfile_path_1 = 'No1/certs/server.key'
+
+certfile_path_2 = 'certs/server.crt'
+keyfile_path_2 = 'certs/server.key'
+
+if os.path.isfile(certfile_path_1) and os.path.isfile(keyfile_path_1):
+    certfile = certfile_path_1
+    keyfile = keyfile_path_1
+elif os.path.isfile(certfile_path_2) and os.path.isfile(keyfile_path_2):
+    certfile = certfile_path_2
+    keyfile = keyfile_path_2
+else:
+    raise FileNotFoundError("Tidak ditemukan.")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))
